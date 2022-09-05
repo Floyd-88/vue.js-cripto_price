58e02fd58a8a8156b9fe86ee084e692a0f50a920
Floyd-88 <ilasaz1988@gmail.com>
2022-09-05 18:07:15 +0300

    save localstorage, color click ticker, load price, dinamic graph

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
             <div class="mt-1 relative rounded-md shadow-md">
               <input
                   v-model="ticker"
-                  @keydown.enter = "addTickers"
+                  @keydown.enter="addTickers"
                   type="text"
                   name="wallet"
                   id="wallet"
@@ -28,7 +28,7 @@
           </div>
         </div>
         <button
-            @:click = "addTickers()"
+            @:click="addTickers()"
             type="button"
             class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
         >
@@ -51,63 +51,67 @@
 
       <hr class="w-full border-t border-gray-600 my-4"/>
       <template v-if="tickers.length">
-      <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-        <div
-            v-for = "elem in tickers"
-            :key = "elem.name"
-            class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
-        >
-          <div class="px-4 py-5 sm:p-6 text-center">
-            <dt class="text-sm font-medium text-gray-500 truncate">
-              {{elem.name}} - USD
-            </dt>
-            <dd class="mt-1 text-3xl font-semibold text-gray-900">
-              {{elem.price}}
-            </dd>
-          </div>
-          <div class="w-full border-t border-gray-200"></div>
-          <button
-              @:click = "deleteTicker(elem)"
-              class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
+        <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+          <div
+              v-for="elem in tickers"
+              :key="elem.name"
+              @click = showGraph(elem)
+              :class="{
+                'border-4':  graph === elem
+              }"
+              class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
           >
-            <svg
-                class="h-5 w-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="#718096"
-                aria-hidden="true"
+            <div class="px-4 py-5 sm:p-6 text-center">
+              <dt class="text-sm font-medium text-gray-500 truncate">
+                {{ elem.name }} - USD
+              </dt>
+              <dd class="mt-1 text-3xl font-semibold text-gray-900">
+                {{ elem.price }}
+              </dd>
+            </div>
+            <div class="w-full border-t border-gray-200"></div>
+            <button
+                @:click.stop="deleteTicker(elem)"
+                class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
             >
-              <path
-                  fill-rule="evenodd"
-                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                  clip-rule="evenodd"
-              ></path>
-            </svg>
-            Удалить
-          </button>
-        </div>
-      </dl>
-      <hr class="w-full border-t border-gray-600 my-4"/>
+              <svg
+                  class="h-5 w-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="#718096"
+                  aria-hidden="true"
+              >
+                <path
+                    fill-rule="evenodd"
+                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                    clip-rule="evenodd"
+                ></path>
+              </svg>
+              Удалить
+            </button>
+          </div>
+        </dl>
+        <hr class="w-full border-t border-gray-600 my-4"/>
       </template>
-      <section class="relative">
+      <section
+          v-if = "graph"
+               class="relative">
+
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          VUE - USD
+          {{graph.name}} - USD
         </h3>
         <div class="flex items-end border-gray-600 border-b border-l h-64">
           <div
+              v-for = "(bar, idx) in normalizeGraph"
+              :key = "idx"
+              :style="{
+                'height': `${bar}%`
+              }"
               class="bg-purple-800 border w-10 h-24"
-          ></div>
-          <div
-              class="bg-purple-800 border w-10 h-32"
-          ></div>
-          <div
-              class="bg-purple-800 border w-10 h-48"
-          ></div>
-          <div
-              class="bg-purple-800 border w-10 h-16"
           ></div>
         </div>
         <button
+            @click = "graph = null"
             type="button"
             class="absolute top-0 right-0"
         >
@@ -146,34 +150,81 @@ export default {
     return {
       ticker: "",
       tickers: [],
+      graph: null,
+      graph_elem: []
     }
+  },
+  created() {
+  let loadLocalStorage = localStorage.getItem("save_tickers")
+    this.tickers = JSON.parse(loadLocalStorage)
+    this.tickers.map(i => this.loadPriceCoins(i))
+
   },
 
   computed: {
-    double_ticker() {
+    doubleTicker() {
       return this.tickers.some(i => i.name === this.ticker.toUpperCase())
+    },
+
+    normalizeGraph() {
+      let max = Math.max(...this.graph_elem)
+      let min = Math.min(...this.graph_elem)
+      if(max === min) {
+        return this.graph_elem.map(()=> 50)
+      }
+      return this.graph_elem.map(i => 5 + (i - min) * 95 / (max - min))
     }
+
   },
   methods: {
+
+    loadPriceCoins(newTicker) {
+      setInterval(
+          async () => {
+            let param = await  fetch(`https://min-api.cryptocompare.com/data/price?fsym=${newTicker.name}&tsyms=USD&api_key=de17c513b59215152bd4fc1a150ae8a440f98184a93654bcbf936560fcec4d0a`)
+            let data = await param.json()
+            this.tickers.find(i => i.name === newTicker.name).price = data.USD > 1 ? data.USD.toFixed(2) :data.USD.toPrecision(2)
+
+            if(this.graph?.name === newTicker.name) {
+           this.graph_elem.push(data.USD)
+            }
+
+          }
+          , 3000)
+    },
+
     addTickers() {
-      if(!this.double_ticker) {
+      if (!this.doubleTicker) {
         let newTicker = {name: this.ticker.toUpperCase(), price: "-"}
         this.tickers = [...this.tickers, newTicker]
+        this.loadPriceCoins(newTicker)
 
         this.ticker = ""
       }
     },
 
     deleteTicker(elem) {
-      if(elem) {
+      if (elem) {
         this.tickers = this.tickers.filter(i => i !== elem)
 
+        this.graph = null
       }
+    },
+
+    showGraph(elem) {
+      this.graph = elem
+
     },
 
   },
   watch: {
+    tickers() {
+      localStorage.setItem("save_tickers", JSON.stringify(this.tickers))
+    },
 
+  graph() {
+    this.graph_elem = []
+  },
   }
 }
 </script>
